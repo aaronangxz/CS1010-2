ba1b85d9e38cd1ff21cc5ac652a254fb0f4aaa98
aaronangxz <angxze@gmail.com>
2021-01-28 22:06:05 +0800

    Updated Tutorial01 Q02

--- Tutorial/Tutorial01/matmul.cpp
+++ Tutorial/Tutorial01/matmul.cpp
@@ -52,14 +52,27 @@ int main()
 void matmul_one_element( int A[][P], int B[][N], int C[][N], 
                         int i, int j) 
 {
+    //Given 2 matrix amd 1 result matrix
+    //Given i,j -> specific cell
+    //Results = col * row, hence run P times
     for (int k = 0; k < P; k++)
     {
+        //Put results into specific cell [i][j]
         C[i][j] += A[i][k] * B[k][j];
     }
-    
-    
 }
 
 void matmul ( int A[][P], int B[][N], int C[][N])
 {
+    //Have to travel across all cells to put in results
+    //Hence M, N for respective cols and rows
+
+    for (int i = 0; i < M; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            //Previous function will put result in C[M][M]
+            matmul_one_element(A,B,C,i,j);
+        }
+    }
 }
