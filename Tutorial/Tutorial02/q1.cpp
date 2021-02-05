0cbdc0d6aca576e727e4d8578a4cc90d0535f007
aaronangxz <angxze@gmail.com>
2021-02-05 11:27:26 +0800

    Tutorial 02 Q01,Q02

--- Tutorial/Tutorial02/q1.cpp
+++ Tutorial/Tutorial02/q1.cpp
@@ -5,24 +5,44 @@ using namespace std;
 
 int power(int base, int exponent)
 {
-	return 0;   //Remember to change the return value too
+	//Base case
+	if (exponent == 1) return base;
+	return base * power(base,exponent - 1);
 
 }
 
 void print_prime_factor( int number )
 {
+	//Base casw
+	if (number == 1) 
+	{
+		cout << endl;
+		return;
+	}
+	//Starts checking from 2 onwards
+	int i = 2;
+	//When there is remainder, check next
+	while(number % i != 0)
+	{
+		i ++;
+	}
+	//Otherwise print out factor
+	cout << i << " ";
+	//Begin next
+	print_prime_factor(number / i);
 }
 
 int min_element(int array[], int N)
 {
-	return 0;   //Remember to change the return value too
+	
+	return 0;
 }
 
 int main()
 {
 	cout << "2^5 = " << power(2, 5) << endl;
 
-	print_prime_factor(7);
+	print_prime_factor(140);
 
 	int array[] = { 6, 7, -1, 3, -9};
 	cout << "Min = " <<  min_element(array, 5) << endl;
--- Tutorial/Tutorial02/q2.cpp
+++ Tutorial/Tutorial02/q2.cpp
@@ -5,14 +5,10 @@ using namespace std;
 
 int climb_stair(int N)
 {
- 	if (N == 1 || N == 0)
-        return 1;
-    else if (N == 2)
-        return 2;
-    else
-        return climb_stair(N - 3) + climb_stair(N - 2)+ climb_stair(N - 1);
+ 	if (N == 1 || N == 0) return 1;
+    else if (N == 2) return 2;
+    else return climb_stair(N - 3) + climb_stair(N - 2)+ climb_stair(N - 1);
 }
-
 
 int main()
 {
