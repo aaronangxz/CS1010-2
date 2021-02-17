5b85d06d02d86e5667e7b7b1b830a4dd28be04e5
aaronangxz <angxze@gmail.com>
2021-02-17 22:12:02 +0800

    Updated AY1920 PE1 PYP task02

--- PYP/Past Year PE1 (AY1920)/AY1920-PE1-task2.cpp
+++ PYP/Past Year PE1 (AY1920)/AY1920-PE1-task2.cpp
@@ -27,10 +27,67 @@ void print_trail(char region[MAXROW][MAXCOL], int nRow, int nCol)
     printf("\n");
 }
 
+void flyRIGHT (char region[MAXROW][MAXCOL],int row)
+{
+    for (int i = 0; i < MAXCOL ; i++)
+    {
+        if(region[row][i] == EMPTY || region[row][i] == RIGHT) region[row][i] = RIGHT;
+        else region[row][i] = CROSS;
+    }
+}
+
+void flyLEFT (char region[MAXROW][MAXCOL],int row)
+{
+    for (int i = MAXCOL - 1; i >= 0; i--)
+    {
+        if(region[row][i] == EMPTY || region[row][i] == LEFT) region[row][i] = LEFT;
+        else region[row][i] = CROSS;
+    }
+}
+
+void flyUP (char region[MAXROW][MAXCOL],int col)
+{
+    for (int i = MAXROW - 1; i >= 0; i--)
+    {
+        if(region[i][col] == EMPTY || region[i][col] == UP ) region[i][col] = UP;
+        else region[i][col] = CROSS;
+    }
+}
+
+void flyDOWN (char region[MAXROW][MAXCOL],int col)
+{
+    for (int i = 0; i < MAXROW; i++)
+    {
+        if(region[i][col] == EMPTY || region[i][col] == DOWN) region[i][col] = DOWN;
+        else region[i][col] = CROSS;
+    }
+}
+
 void plot_trail(char region[MAXROW][MAXCOL])
 {
-    //Add your code below.
-    //Introduce helper function to simplify your coding.
+    for (int row = 0; row < MAXROW; row++)
+    {
+        if (region[row][0] == RIGHT)
+        {
+            flyRIGHT(region,row);
+        }
+        if (region[row][MAXCOL - 1] == LEFT)
+        {
+            flyLEFT(region,row);
+        }
+    }
+
+    for (int col = 0; col < MAXCOL; col++)
+    {
+        if (region[0][col] == DOWN)
+        {
+            flyDOWN(region,col);
+        }
+        if (region[MAXROW - 1][col] == UP)
+        {
+            flyUP(region,col);
+        }
+    }
 }
 
 int main()
