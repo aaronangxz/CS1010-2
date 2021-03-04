27c7ef7ba0ffec310d3d354781f36e2d6b33a2bc
aaronangxz <angxze@gmail.com>
2021-03-04 13:26:02 +0800

    Updated Tutorial 03

--- Tutorial/Tutorial03/Q4-Template.cpp
+++ Tutorial/Tutorial03/Q4-Template.cpp
@@ -9,27 +9,40 @@ struct Fraction {
 };         
 
 
-void readFraction( Fraction *fptr );
+void readFraction( Fraction& fRef );
 //Purpose: Read numerator and denominator into fraction structure
-//Output: The fraction structure referred by fptr is updated
+//Output: The fraction structure referred by fRef is updated
 //Return: None
 
-void printFraction( Fraction *fptr );
+void printFraction( Fraction& fRef );
 //Purpose: Print numerator and denominator in a fraction structure
-//Input: The fraction structure referred by fptr is printed
+//Input: The fraction structure referred by fRef is printed
 //Return: None
 
 //TODO: You need to modify this version to sort Fraction instead
-void insertionSort(int a[], int n)
+int compare(Fraction f1, Fraction f2)
+{
+    int nF1num, nF2num;
+    nF1num = f1.num * f2.den;
+    nF2num = f2.num * f1.den;
+
+    if (nF1num < nF2num) return -1;
+    if (nF1num == nF2num) return 0;
+    return 1;
+}
+
+void insertionSort(Fraction a[], int n)
 //Purpose: Perform insertion sort on a[0...n-1]
 //Output: The a[] will be sorted
 //Return: None
 {
-    int i, j, next;
+    int i, j;
+    Fraction next;
 
-    for (i=1; i<n; i++) {
+    for (i = 1; i < n; i++) {
         next = a[i];
-        for (j=i-1; j>=0 && a[j]>next; j--){
+        for (j = i - 1; j >= 0 && (compare(a[j],next)) == 1; j--)
+        {
             a[j+1] = a[j];
         }
         a[j+1] = next;
@@ -45,22 +58,19 @@ int main()
     //declare an array of 5 fractions
     Fraction fA[5] = {{2,5}, {4,10}, {1,20}, {2,10}, {1,5}};
 
-    //printFraction(&(fA[0]));
-
-    //read 5 fractions
+    //Uncomment to read 5 fractions
     //for (i = 0; i < 5; i++){
-    //    readFraction( &(fA[i]) );
+    //    readFraction( fA[i] );
     //}
 
     //Calll insertion sort after you've updated it to sort Fractions
-
+    insertionSort(fA,5);
     //print the sorted fractions
-    for (i = 0; i < 5; i++){
-        printFraction( &(fA[i]) );
+    for (i = 0; i < 5; i++)
+    {
+        printFraction( fA[i] );
         cout << endl;
     }
-
-
 
     return 0;
 }
