fe06d85d349919010f91897ddab7dd520954ac76
aaronangxz <angxze@gmail.com>
2021-03-29 23:15:44 +0800

    Updated Lec07 Q6

--- Lecture/Lecture7/Q6.cpp
+++ Lecture/Lecture7/Q6.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-
+#include <limits.h>
 using namespace std;
 
 template <typename T>
@@ -15,9 +15,7 @@ bool valid_BST(BSTNode<T> &root)
     if(root.left != NULL && root.left->item <= root.item) return 0;
     if(root.right != NULL && root.right->item >= root.item) return 0;
     
-    return valid_BST(root.left) and valid_BST(root.right);
-    // your code here
-    
+    return valid_BST(root.left) and valid_BST(root.right);    
 }
 
 template <typename T>
@@ -71,4 +69,39 @@ bool valid_BST(BSTNode<T> &root)
 {
     BSTNode<T> *prev = NULL;
     return valid_BST_util(root, prev);
+}
+
+template <typename T>
+bool isValidBST(BSTNode<T> &root, BSTNode<T> &minNode, BSTNode<T> &maxNode) {
+    if(!root) return true;
+    if(minNode && root.item <= minNode.item || maxNode && root.item >= maxNode.item)
+        return false;
+    return isValidBST(root.left, minNode, root) && isValidBST(root.right, root, maxNode);
+}
+
+template <typename T>
+bool valid_BST(BSTNode<T> &root) {
+    return isValidBST(root, NULL, NULL);
+}
+
+
+template <typename T>
+bool isValidBSTAdd(BSTNode<T> &root, long min, long max) 
+{
+    if(root==NULL)
+    {
+        return true;
+    }
+    if(root.item >min && root.item <max)
+    {
+        return isValidBSTAdd(root.left ,min, root.item) && isValidBSTAdd(root.right ,root.item ,max);
+    }
+    return false;
+}
+
+template <typename T>
+bool valid_BST(BSTNode<T> &root) 
+{
+      
+    return isValidBSTAdd(root,LONG_MIN, LONG_MAX);
 }
