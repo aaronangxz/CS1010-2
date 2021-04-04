694aee60de6e2eb77d7beca5030d003f7c03d2a0
aaronangxz <angxze@gmail.com>
2021-04-04 21:39:19 +0800

    Updated Pset03

--- ProblemSets/ProblemSet3/ps3-template.cpp
+++ ProblemSets/ProblemSet3/ps3-template.cpp
@@ -34,6 +34,7 @@ bool match_bracket(string expression)
         switch (expression[i]) 
         { 
         case ')': 
+            if (s.empty()) return false; 
             cout << "')' detected." << endl;
 
             // Store the top element in a 
@@ -48,6 +49,7 @@ bool match_bracket(string expression)
             break; 
   
         case '}': 
+            if (s.empty()) return false; 
             cout << "'}' detected." << endl;
 
             // Store the top element in b 
@@ -62,6 +64,7 @@ bool match_bracket(string expression)
             break; 
   
         case ']': 
+            if (s.empty()) return false; 
             cout << "']' detected." << endl;
 
             // Store the top element in c 
@@ -95,14 +98,16 @@ struct Token {
 Token make_token(int i) {
     Token t;
     // your code here
-    t._operator = i;
+    t._operand = i;
+    t._operator = '\0';
     return t;
 }
 
 Token make_token(char op) {
     Token t;
     // your code here
-    t._operand = op;
+    t._operator = op;
+    t._operand = 0;
     return t;
 }
 
@@ -117,7 +122,7 @@ int get_opnd(Token t) {
 }
 
 bool is_opnd(Token t) {
-    if (t._operand >= 0 && t._operand <= 9) return true;
+    if (t._operand != 0) return true;
     else return false;
     // your code here
 }
@@ -129,17 +134,23 @@ bool is_opnd(Token t) {
 vector<Token> tokenize(string expression) {
     vector<Token> output;
     string i;
-    for (char c : expression) {
+    for (char c : expression) 
+    {
         // if c is a digit
-        if (isdigit(c)) {
+        if (isdigit(c)) 
+        {
             i += c;
-        } else {
-            if (!i.empty()) {
+        } 
+        else 
+        {
+            if (!i.empty()) 
+            {
                 output.push_back(make_token(stoi(i)));
                 i.clear();
             }
             // convert brackets to round
-            switch (c) {
+            switch (c) 
+            {
                 case '[':
                 case '{':
                     c = '(';
@@ -167,7 +178,7 @@ int priority(Token t) {
         case '+':
         case '-':
             return 0;
-        case '*':
+        case '*':       
         case '/':
             return 1;
         case '^':
@@ -175,7 +186,6 @@ int priority(Token t) {
     }
     return -1;
 }
-
 // Function to teach cout how to display vector of Token
 ostream & operator << (ostream &out, const Token &t) {
     if (is_opnd(t))
@@ -199,6 +209,81 @@ ostream & operator << (ostream &out, const vector<Token> &v)
 //*************************
 vector<Token> in_to_post(vector<Token> infix) {
     vector<Token> output;
+    stack<Token> s;
+
+    for(int i = 0; i < infix.size(); i++)
+    {
+        if(get_optr(infix[i]) == ' ') continue;
+        //if token is operand
+        if(is_opnd(infix[i]))
+        {
+            output.push_back(infix[i]);
+            cout << "output operand " << infix[i]._operand << endl;
+        }
+        //if token is open bracket
+        else if (get_optr(infix[i]) == '(')
+        {
+            s.push(infix[i]);
+            cout << "push ( to s" << endl;
+        }
+        //if token is operator
+         //if token is open bracket
+        else if (get_optr(infix[i]) == ')')
+        {
+            cout << "found )" << endl;
+            while(get_optr(s.top()) != '(')
+            {
+                cout << "output "<< s.top()._operator << endl;
+                output.push_back(s.top());
+                s.pop();
+            }
+            cout << s.top()._operator << endl;
+            // s.pop();            
+        }
+        else if (!is_opnd(infix[i]))
+        {
+            cout << "found optr " << infix[i]._operator << endl;
+            if(s.empty())
+            {
+                s.push(infix[i]);
+                continue;
+            }
+            //If stack is not empty and either top of stack is brackets or precedence of stack is lower
+            else
+            {
+                if(get_optr(s.top()) == '(' || get_optr(s.top()) == ')' || (priority(infix[i]) > priority(s.top())))
+                {
+                    // if(get_optr(s.top()) == '(') s.pop();
+                    cout << "push optr " << infix[i]._operator << " to s"<< endl;
+                    s.push(infix[i]);
+                }
+                //Otherwise pop until satisfy requirements
+                else
+                {
+                    while(get_optr(s.top()) != '(' || get_optr(s.top()) != ')' || (priority(infix[i]) < priority(s.top())))
+                    {
+                        
+                        cout << "pop " << s.top()._operator << " from s " << endl;
+                        s.pop();
+                    }   
+                    output.push_back(infix[i]);
+                    cout << "push " << infix[i] << endl;
+                }
+            } 
+        }
+    }
+
+    while(!s.empty())
+    {
+        Token x = s.top();
+
+        if(get_optr(x) == '(' || get_optr(x) == ' ') s.pop();
+        else
+        {
+            output.push_back(x);
+            s.pop();
+        }
+    }
     // your code here
 
     return output;
@@ -209,9 +294,48 @@ vector<Token> in_to_post(vector<Token> infix) {
 // Task 4: Calculate the expression
 //*********************************
 #include <math.h>
-int calculate(vector<Token> postfix) {
-    // replace with your code
-    return 0;
+int calculate(vector<Token> postfix) 
+{
+    stack<int> s;
+    int results = 0;
+    for(int i = 0; i < postfix.size() ; i++)
+    {
+        if(is_opnd(postfix[i])) s.push(get_opnd(postfix[i]));
+        else 
+        {
+            int a = s.top();
+            s.pop();
+            int b = s.top();
+            s.pop();
+
+            switch (get_optr(postfix[i])) 
+            {
+                case '+':
+                    results = a + b;
+                    cout << a << "&" << b << "(+) result is " << results << endl;
+                    break;
+                case '-':
+                    results = a - b;
+                    cout << a << "&" << b << "(-) result is " << results << endl;
+                    break;
+                case '*':
+                    results = a * b;
+                    cout << a << "&" << b << "(*) result is " << results << endl;
+                    break;
+                case '/':
+                    results = a / b;
+                    cout << a << "&" << b << "(/) result is " << results << endl;
+                    break;
+                case '^':
+                    results = pow(b,a);
+                    cout << a << "&" << b << "(^) result is " << results << endl;
+                    break;
+            } 
+            s.push(results);
+        }
+    }
+    
+    return s.top();
 }
 
 
@@ -223,26 +347,26 @@ int main(void) {
     cout << boolalpha;  // tell cout to display bool as true/false
 
     // Task 1
-    // cout << match_bracket("1+2*[3**(1-2)]-{1/4}") << endl;
-    // cout << match_bracket("1 + 2 * [ 3 ** { 1 - 2) ] - { 1 / 4 }") << endl;
-    // cout << match_bracket("1+(2*3)**4") << endl;
-    // cout << match_bracket("(1+[2-3]*{9/2})") << endl;
-    // cout << match_bracket("(1+[2-3]*9/2})") << endl;
+    // cout << match_bracket("1 + ( 2 * 3 ) ** 4") << endl;
+    // cout << match_bracket("1 + 2 * [ 3 ** ( 1 - 2) ] - { 1 / 4 }") << endl;
+    // cout << match_bracket("3 + ( 2 - [ 5 / 3 ] - ) - }") << endl;
 
     // You may edit the expression for testing
     // Remember only non-negative integers are supported
-    string expression = "1+((2+3*4)/5)^6%7";
+    string expression = "1 + ( 2 * 3 ) ^ 4";
+    // string expression = "(1+2)*3^4";
 
-    // // Task 2
+
+    // // // Task 2
     vector<Token> infix = tokenize(expression);
     cout << "   Expr: " << expression << endl;
     cout << "  Infix: " << infix << endl;
 
-    // // Task 3
-    // vector<Token> postfix = in_to_post(infix);
-    // cout << "Postfix: " << postfix << endl;
+    // // // Task 3
+    vector<Token> postfix = in_to_post(infix);
+    cout << "Postfix: " << postfix << endl;
 
-    // // Task 4
-    // cout << " Result: ";
-    // cout << calculate(postfix) << endl;
+    // // // Task 4
+    cout << " Result: ";
+    cout << calculate(postfix) << endl;
 } 
