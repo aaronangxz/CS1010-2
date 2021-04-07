3ee100bc0ae239b9faa028c32ac082453ba72f56
aaronangxz <angxze@gmail.com>
2021-04-07 22:16:12 +0800

    Updated Tutorial07 Q2

--- Tutorial/Tutorial07/Tutorial07-Q2.cpp
+++ Tutorial/Tutorial07/Tutorial07-Q2.cpp
@@ -29,16 +29,7 @@ public:
     ~OldMcDonald() {
         _farm.clear();
     }
-    
-    // void sing() {
-    //     for (int i=0; i<_farm.size(); i++) {
-    //         cout << "Old McDonald had a farm, E-I-E-I-O\n";
-    //         cout << "And on his farm he had a " << _farm[i]->get_species() << ", E-I-E-I-O" << endl;
-    //         cout << "With a " << _farm[i]->get_sound() << " " << _farm[i]->get_sound()<< " here and a " << _farm[i]->get_sound() << " " << _farm[i]->get_sound() << " there" << endl;
-    //         cout << _farm[i]->get_sound() << " here " << _farm[i]->get_sound() << " there, everywhere " << _farm[i]->get_sound() << " " << _farm[i]->get_sound() << endl;
-    //         cout<< "Old McDonald had a farm, E-I-E-I-O" << endl << endl;
-    //     }
-    // }
+
     void sing() {
         for (int i=0; i<_farm.size(); i++) {
             cout << "Old McDonald had a farm, E-I-E-I-O\n";
